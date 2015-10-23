b4882d4bcb791f6c4a0fdbe04aaed28778ea24b3
Martins Okuonghae <martins.okuonghae@gmail.com>
2015-10-23 04:42:18 +0100

    Php Cms Commit

--- content.php
+++ content.php
@@ -22,21 +22,27 @@
 
 if (isset($_GET['info'])) //grab value!
 {
-	$table1 = $_GET['info']; // so if this has a set create the variable
-	$table2="";
+	$Sel_table1 = get_info_by_id($_GET['info']);
+
+	$sel_t2 =0;
+	$table2 =  NULL;
 }
 else if (isset($_GET['page'])) //else if for our pages
 {
-	$table1="";
-	$table2 = $_GET ['page'];
+	$table1=0;
+	$Sel_table1 = NULL;
+	$table2 = get_pages_by_id($_GET['page']);
 	
 }
 else{ //final else statement because there is more than 2 things that can happen
 
-   $table1 = "";
-   $table2 = "";
+   $table1 = 0;
+   $Sel_table1 = NULL;
+   $table2 = 0;
 
 }
+
+
 
 ?>
 
@@ -66,15 +72,32 @@ else{ //final else statement because there is more than 2 things that can happen
 
 
 while ($info = mysql_fetch_array($info_set)){
-echo "<li> <a href = \"content.php?info=" . urlencode($info["id"]) . "\">{$info["menu"]}</a></li>"; //in line substitution
+
+
+ echo "<li";
+
+	if($info["id"] == $Sel_table1['id'])
+	{
+ echo " class=\"selected\"";
+}
+ echo "> <a href = \"content.php?info= " . urlencode($info["id"]) . "\">{$info["menu"]}</a></li>"; //in line substitution
 
 
 
 
 $page_set= get_pages_for_info($info["id"]);
+
+
 echo "<ul class=\"pages\">";
+
 while ($page = mysql_fetch_array($page_set)){
-echo "<li> <a href =\"content.php?page = " .urlencode($page["id"]). "
+
+echo "<li";
+if ($page["id"]== $table2['id'])
+{
+echo " class=\"selected\"";
+}
+echo "><a href =\"content.php?page = " .urlencode($page["id"]). "
 \">{$page["menu"]}</a></li>"; //in line substitution
 
 
@@ -86,14 +109,37 @@ echo "</ul>";
 
 
 </ul>
+
+<br><br>
+<div id="new">
+<a href = "new_info.php">Add New Information</a>
+
+</div>
 </td>
 
 <td id= "main">
 
-<h2> Main Content</h2>
+<?php if (!is_null($Sel_table1)) { ?>
 
- <?php echo $table1; ?><br>
- <?php echo $table2; ?>
+<h2><?php echo $Sel_table1['menu']; ?></h2>
+
+<?php} elseif (!is_null($table2)) { ?>
+
+<h2><?php echo $table2 ['menu']; ?></h2>
+
+<div class="page-content">
+
+ <?php
+
+  echo $table2['content'] ;
+
+ ?>
+
+</div>
+
+<?php } else { ?>
+<h2>Select a menu from our information table or page table</h2>
+<?php } ?>
 
  </td>
 
@@ -109,4 +155,3 @@ echo "</ul>";
 require("includes/footer.php");
 
 ?>
-
--- includes/functions.php
+++ includes/functions.php
@@ -34,8 +34,50 @@ function confirm_query($result_set){
 		return $page_set;
 		
 	}
+	
+	function get_info_by_id($information_id)
+	{
+		global $connection;
+		$query_3 = "SELECT * ";
+		$query_3 .= "FROM information "; //Append equals, so the value can change
+		$query_3 .= "WHERE id= " .$information_id;
+		$query_3 .= " LIMIT 1 ";  //Mysql command to limit results by 1
+		
+		$result_set = mysql_query($query_3, $connection);
+		confirm_query ($result_set);
+		
+		if ($info = mysql_fetch_array($result_set)){
+		return $info;
+		}
+		else
+		{ 
+		return NULL;
+		
+		}
+	}
 
-
+	function get_pages_by_id($page_id)
+	{
+	
+		global $connection;
+		$query_3 = "SELECT *";
+		$query_3 .= "FROM pages "; //Append equals, so the value can change
+		$query_3 .= "WHERE id=" .$page_id;
+		$query_3 .= " LIMIT 1";  //Mysql command to limit results by 1
+		
+		$result_set = mysql_query($query_3, $connection);
+		confirm_query($result_set);
+		
+		if ($page = mysql_fetch_array($result_set)){
+		return $page;
+		}
+		else
+		{ 
+		return NULL;
+		
+		}
+	}
+	
 
 
 ?>
